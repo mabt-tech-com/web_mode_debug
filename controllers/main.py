8ed52b8e98d994c5a014952572af04ed36fa056f
Med Amine bt <60607576+iifast2@users.noreply.github.com>
2024-08-20 15:25:49 +0100

    Update main.py

--- controllers/main.py
+++ controllers/main.py
@@ -4,14 +4,14 @@ from odoo import http
 from odoo.addons.web.controllers.main import Home
 
 
-class Home(Home):
+class CustomHome(Home):
 
     @http.route()
     def web_login(self, redirect=None, **kw):
-        res = super().web_login(redirect, **kw)
-        uid = http.request.uid
+        res = super(CustomHome, self).web_login(redirect, **kw)
+        uid = http.request.session.uid
         if uid:
             user = http.request.env['res.users'].sudo().browse(uid)
-            if user._is_admin() or user._is_superuser():
+            if user.has_group('base.group_system'):  # Checks if the user belongs to the Administrator group
                 http.request.session['debug'] = 'assets'
         return res
